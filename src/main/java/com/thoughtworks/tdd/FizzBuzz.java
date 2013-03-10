fd6a60798d5592dab5aa4231f5ead4f5835ff77f
sghill <sghill.dev@gmail.com>
2013-03-10 14:48:52 -0400

    passing simplest way possible

--- src/main/java/com/thoughtworks/tdd/FizzBuzz.java
+++ src/main/java/com/thoughtworks/tdd/FizzBuzz.java
@@ -2,6 +2,6 @@ package com.thoughtworks.tdd;
 
 public class FizzBuzz {
     public String say(int i) {
-        throw new UnsupportedOperationException("generated method not yet implemented.");
+        return "2";
     }
 }
