6a04616e817cb8570ce9018cf11dd6918e1eb10d
sghill <sghill.dev@gmail.com>
2013-03-10 15:07:37 -0400

    fizz scenario passes simplest way possible

--- src/main/java/com/thoughtworks/tdd/FizzBuzz.java
+++ src/main/java/com/thoughtworks/tdd/FizzBuzz.java
@@ -2,6 +2,9 @@ package com.thoughtworks.tdd;
 
 public class FizzBuzz {
     public String say(int number) {
+        if(number == 3) {
+            return "fizz";
+        }
         return String.valueOf(number);
     }
 }
