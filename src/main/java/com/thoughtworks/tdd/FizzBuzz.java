ef58f9541f52655bf523b87b24e0d6d19ee79eef
sghill <sghill.dev@gmail.com>
2013-03-10 15:32:35 -0400

    returning fizzbuzz for 15

--- src/main/java/com/thoughtworks/tdd/FizzBuzz.java
+++ src/main/java/com/thoughtworks/tdd/FizzBuzz.java
@@ -3,7 +3,11 @@ package com.thoughtworks.tdd;
 public class FizzBuzz {
     public String say(int number) {
         if(isMultiple(number, 3)) {
-            return "fizz";
+            if(isMultiple(number, 5)) {
+                return "fizzbuzz";
+            } else {
+                return "fizz";
+            }
         }
         if(isMultiple(number, 5)) {
             return "buzz";
