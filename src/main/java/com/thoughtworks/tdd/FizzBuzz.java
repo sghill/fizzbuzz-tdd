e29071b5dcabaf0a8ca7cf21943c99a34661a22f
sghill <sghill.dev@gmail.com>
2013-03-10 15:11:49 -0400

    any multiple of three returns fizz

--- src/main/java/com/thoughtworks/tdd/FizzBuzz.java
+++ src/main/java/com/thoughtworks/tdd/FizzBuzz.java
@@ -2,7 +2,7 @@ package com.thoughtworks.tdd;
 
 public class FizzBuzz {
     public String say(int number) {
-        if(number == 3) {
+        if(number % 3 == 0) {
             return "fizz";
         }
         return String.valueOf(number);
