f992cb0dea71223024a742a96bcaf367a46ea399
sghill <sghill.dev@gmail.com>
2013-03-10 15:23:40 -0400

    any multiple of five returns buzz

--- src/main/java/com/thoughtworks/tdd/FizzBuzz.java
+++ src/main/java/com/thoughtworks/tdd/FizzBuzz.java
@@ -5,7 +5,7 @@ public class FizzBuzz {
         if(number % 3 == 0) {
             return "fizz";
         }
-        if(number == 5) {
+        if(number % 5 == 0) {
             return "buzz";
         }
         return String.valueOf(number);
