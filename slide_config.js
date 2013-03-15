d3cef8e6cfabc25c1bc262fbd159fb53c2f016b6
sghill <sghill.dev@gmail.com>
2013-03-14 20:36:29 -0700

    adding slides, based on the excellent google io-2012-slides

---
+++ slide_config.js
@@ -0,0 +1,24 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Test-Driven Design',
+    useBuilds: true,
+    usePrettify: true,
+    enableSlideAreas: true,
+    enableTouch: true,
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ]
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Bahador Biglari',
+    company: 'Developer, ThoughtWorks'
+  }, {
+    name: 'Steve Hill',
+    company: 'Developer, ThoughtWorks'
+  }]
+};
+
